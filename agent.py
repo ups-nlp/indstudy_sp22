0ab9b08f78b7f6afac5cada62001cf1cf447f379
annavanboven <anna.van.boven@gmail.com>
2022-02-08 13:45:33 -0800

    add functions for parallel in the mcts agent file

--- agent.py
+++ agent.py
@@ -23,6 +23,49 @@ class RandomAgent(Agent):
         valid_actions = env.get_valid_actions()
         return random.choice(valid_actions)
 
+class MonteAgent(Agent):
+    node_path = []
+
+    def __init__(self, env: FrotzEnv, game_file:str, num_steps: int):
+
+        action_values = {}
+        action_counter = {}
+        self.take_time = True
+        #create threads
+        # for each thread, give them a game to play with:
+        envThing = FrotzEnv(game_file)
+
+    def take_action(self, env: FrotzEnv, history: list) -> str:
+        """Takes in the history and returns the next action to take"""
+        print("Action: ")
+        #
+        # Train the agent using the Monte Carlo Search Algorithm
+        #
+
+        #current number of generated nodes
+        count = 0
+
+        # time at sim start
+        start_time = time.time()
+
+        # how many seconds have elapsed since sim start
+        seconds_elapsed = 0
+
+        # loose time limit for simulation phase
+        time_limit = 10
+
+        # minimum number of nodes per simulation phase
+        minimum = env.get_moves()*5
+
+        #spin threads to begin their process
+        while((seconds_elapsed < time_limit or count <= minimum)):
+           #wait here
+           x = 1
+        self.take_time = False
+        
+        
+
+
 
 class HumanAgent(Agent):
     """Allows a human player"""
