c511eba897a38cb44a603182bf20003b48ea2774
americachambers <alchambers@pugetsound.edu>
2022-06-13 12:28:02 -0700

    Removing original as an input parameter to default_policy

--- agent.py
+++ agent.py
@@ -93,7 +93,7 @@ class MonteAgent(Agent):
                 print('[TAKE ACTION] Printing out selected child', str(new_node))
 
             # Determine the simulated value of the new node
-            delta = default_policy(new_node, env, self.max_depth, self.alpha, original=True)
+            delta = default_policy(new_node, env, self.max_depth, self.alpha)
             
             # Propogate the simulated value back up the tree
             backup(new_node, delta)
