0fd7ca22dc89aca1b0db5265a873e817346226c7
americachambers <alchambers@pugetsound.edu>
2022-08-03 09:04:09 -0700

    Fixed how the file name was created. It wasn't taking into account s, d, or c

--- evaluate.py
+++ evaluate.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     # Create file name for results file
     file_str = ''
     if args.agent == 'mcts':
-        file_str = f'basicTesting/{game}_{args.agent}_{args.num_trials}t{args.num_moves}m{args.mcts_time}s{args.mcts_depth}d.txt'
+        file_str = f'basicTesting/{game}_{args.agent}_{args.num_trials}t{args.num_moves}m{args.mcts_time}s{args.mcts_depth}d{args.explore_exploit}c.txt'
     else:
         file_str = f'basicTesting/{game}_{args.agent}_{args.num_trials}t{args.num_moves}m.txt'
 
